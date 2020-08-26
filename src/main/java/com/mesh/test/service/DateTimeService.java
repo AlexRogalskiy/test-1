c126d2c68027f733882093173aacbfdabad6d1b3
dvur0g <hoof7@mail.ru>
2020-08-26 23:00:12 +0300

    Не стал делать DateTimeService и ExceptionService бинами для наглядности

--- src/main/java/com/mesh/test/service/DateTimeService.java
+++ src/main/java/com/mesh/test/service/DateTimeService.java
@@ -1,11 +1,8 @@
 package com.mesh.test.service;
 
-import org.springframework.stereotype.Service;
-
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
-@Service
 public class DateTimeService {
     static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
